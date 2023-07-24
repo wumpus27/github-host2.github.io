ef1c6ee8f56e9d890569dc20df7c26dc324f7626
github host <110337548+github-host2@users.noreply.github.com>
2023-07-24 17:26:52 +0200

    2

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.write("<div class='cookie-banner'><span class='material-symbols-outlined'>info</span>This website uses cookies to bring a better experience to you. By using this website you accept the use of cookies for a better experience on this and on our partner websites. Read more in our <a href='privacy.html'>Privacy Policy</a>.<button onclick='set_cs(this);'>Accept</button></div><br><br>");
 
 function access_dashboard() {
+  var setting = document.getElementById("expand-size");
   var access_state = localStorage.getItem("access");
   if (access_state == "true") {
     const elements = document.querySelectorAll("*");
@@ -44,10 +45,11 @@ function object_state() {
 }
 
 function set_es(object) {
+  localStorage.setItem("this_expand-size", object.checked);
   if (object.checked) {
-    document.querySelector(':root').style.setProperty('--dashboardw', 'blue');
+    document.querySelector(':root').style.setProperty('--dashboardw', '500px');
   } else {
-    document.querySelector(':root').style.setProperty('--dashboardh', 'red');
+    document.querySelector(':root').style.setProperty('--dashboardh', '500px');
   }
 }
 
