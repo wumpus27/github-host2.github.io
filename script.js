eb19f340864b2db887eee7d2cc8922aa41f01bd1
github host <110337548+github-host2@users.noreply.github.com>
2023-07-24 17:10:35 +0200

    1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.write("<div class='cookie-banner'><span class='material-symbols-outlined'>info</span>This website uses cookies to bring a better experience to you. By using this website you accept the use of cookies for a better experience on this and on our partner websites. Read more in our <a href='privacy.html'>Privacy Policy</a>.</div><br><br>");
+document.write("<div class='cookie-banner'><span class='material-symbols-outlined'>info</span>This website uses cookies to bring a better experience to you. By using this website you accept the use of cookies for a better experience on this and on our partner websites. Read more in our <a href='privacy.html'>Privacy Policy</a>.<button onclick='set_cs(this);'>Accept</button></div><br><br>");
 
 function access_dashboard() {
   var access_state = localStorage.getItem("access");
@@ -16,16 +16,16 @@ function access_dashboard() {
     if (developer_password == "webseite945") {
       const elements = document.querySelectorAll("*");
       elements.forEach((element) => {
-      element.classList.remove("skeleton");
-      element.classList.remove("hidden");
-      element.disabled = false;
-    });
-    document.getElementById("access").style.display = "none";
-    localStorage.setItem("access", "true");
+        element.classList.remove("skeleton");
+        element.classList.remove("hidden");
+        element.disabled = false;
+      });
+      document.getElementById("access").style.display = "none";
+      localStorage.setItem("access", "true");
     } else {
-    alert("403: Forbidden");
-    localStorage.setItem("access", "false");
-  }
+      alert("403: Forbidden");
+      localStorage.setItem("access", "false");
+    }
   }
 }
 
@@ -36,9 +36,9 @@ function object_state() {
     document.getElementById("cpi-start").hidden = false;
     document.getElementById("cpi-end").hidden = false;
     document.getElementById("cpi-apply").hidden = false;
-   } else {
+  } else {
     document.getElementById("cpi-start").hidden = true;
-  document.getElementById("cpi-end").hidden = true;
+    document.getElementById("cpi-end").hidden = true;
     document.getElementById("cpi-apply").hidden = true;
   }
 }
