4dc339b304447edbe971f90c8aa9c2b5b5647237
github host <110337548+github-host2@users.noreply.github.com>
2023-07-25 20:36:03 +0200

    Update script.js
    Signed-off-by: github host <110337548+github-host2@users.noreply.github.com>

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const text = `HEY! Your task lol is now overdue.`;
 const notification = new Notification("To do list", { body: text, icon: img });
 
 function handleCredentialResponse(response) {
-        const responsePayload = decodeJwtResponse(response.credential)
         localStorage.setItem("access", "true");
         access_dashboard();
     }
